2ace170a7ec0c45e3a691f113e97b32958452622
Vitor Sbeghi <126426164+SbgVit0r@users.noreply.github.com>
2024-04-11 12:11:51 -0300

    Segunda parte do projeto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker(daily_Income,name ) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker(daily_Income,name ) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker(daily_Income,name ) VALUES ('Alex', 250.0);
